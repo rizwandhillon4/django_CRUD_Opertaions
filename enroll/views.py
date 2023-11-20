27cd62b3819314abd6a4491f40be7b61d1f10686
Muhammad Rizwan <m.rizwandhillon4@gmail.com>
2023-11-20 23:31:53 +0500

    here is my third commit form data saved in database

--- enroll/views.py
+++ enroll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 from .forms import StudentRegistration
 # Create your views here.
 
@@ -6,6 +7,16 @@ def add_show(request):
 
     if request.method == "POST":
         fm = StudentRegistration(request.POST)
+        if  fm.is_valid():
+      
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg =User(name=nm, email=em, password=pw,)
+    
+        reg.save()
+        fm = StudentRegistration()
     else:
         fm = StudentRegistration()
-    return render(request, 'enroll/addandshow.html', context={'form':fm})
+        stud = User.objects.all()
+    return render(request, 'enroll/addandshow.html', context={'form':fm, 'stu':stud})
